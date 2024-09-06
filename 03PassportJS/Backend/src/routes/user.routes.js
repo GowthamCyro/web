e745d82035b1726da81a9c57e3c7ea46dd65336b
GowthamCyro <cyrogamer.yt@gmail.com>
2024-09-06 16:58:07 +0530

    Autogenerate Access Token

--- 03PassportJS/Backend/src/routes/user.routes.js
+++ 03PassportJS/Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middleware/mutler.middleware.js";
 import {verifyJWT} from "../middleware/auth.middleware.js"
 
@@ -20,8 +20,10 @@ router.route("/register").post(
     registerUser
 )
 router.route("/login").post(loginUser)
+router.route("/refreshToken").post(refreshAccessToken)
 
 // secured routes
 router.route("/logout").post(verifyJWT,logoutUser);
+
 
 export default router
